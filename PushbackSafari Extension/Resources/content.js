5e36afcb70bd1549653f2910e2a03eda103493c5
UUNEO <uuneo@qq.com>
2024-12-31 07:42:23 +0800

    first

--- PushbackSafari Extension/Resources/content.js
+++ PushbackSafari Extension/Resources/content.js
@@ -5,3 +5,19 @@ browser.runtime.sendMessage({ greeting: "hello" }).then((response) => {
 browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
     console.log("Received request: ", request);
 });
+
+if (location.host.includes("tagram.com")) {
+	const observer = new MutationObserver((mutations) => {
+		mutations.forEach((mutation) => {
+			if (mutation.type === "childList") {
+				const newDivs = document.querySelectorAll("div._aagw");
+				newDivs.forEach((div) => {
+					div.remove();
+				});
+			}
+		});
+	});
+	observer.observe(document.body, { childList: true, subtree: true });
+
+	console.log("Observer started: All divs with class '_aagw' will be removed dynamically.");
+}
